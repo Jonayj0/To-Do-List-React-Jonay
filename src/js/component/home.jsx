755e3a05a3c47851bab0ad47b781880f367c697d
Jonayj0 <141333392+Jonayj0@users.noreply.github.com>
2024-03-08 22:55:46 +0000

    Lista Tareas React Jonay con Boton X

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -6,10 +6,10 @@ const Home = () => {
 
  const [newtarea,setNewtarea]=useState("")
  const [tareas,setTareas]=useState([])
+ 
 
  function writetarea(event) {
 	// console.log(event);
-
 	if (event.key === "Enter") {
 		// console.log("Agregar tarea");
 		setTareas(tareas.concat(newtarea))
@@ -17,23 +17,44 @@ const Home = () => {
 	}
  }
 
+// function deleteTarea(event) {
+// 	console.log(event);
+// }
+
+
 	return (
 		<>
-		<h1 className="text-center mt-5">LISTA DE TAREAS</h1>
+		<h1 className="text-center mt-5"><b>LISTA DE TAREAS</b></h1>
 		<div className="container border">
-			<input className="container border-0 p-3" type="text" onChange={(event) => {setNewtarea(event.target.value)}} 
+			<input className="list container border-0 py-3" type="text" onChange={(event) => {setNewtarea(event.target.value)}} 
 					onKeyDown={writetarea} value={newtarea} placeholder="Añadir nueva tarea"/>
 					
-				<ul className="list-group list-group-flush">
+			<ul className="list-group list-group-flush">
 					{tareas.map((tarea,index) => {
-						return (<li className="list-group-item" key={index}>{tarea}</li>)
+						return (<li className="list-group-item py-3 ms-3" 
+						key={index} onClick={(event) => {}}>{tarea} 
+						<button className="btn text-danger border-0"><i className="fa-solid fa-xmark" ></i></button></li>)
 					}
 					)}	
-				</ul>
-				<div className="contador border-top p-3 text-muted"><span>Tareas diarias</span></div>
+			</ul>
+				<div className="contador border-top p-3"><span>{tareas.length} Tareas</span></div>	
 		</div>
 		</>
 	);
 };
 
 export default Home;
+
+
+
+
+
+			// <ul class="list-group m-5">
+			// 	<li class="list-group-item"><input className="list container border-0" type="text" onChange={(event) => {setNewtarea(event.target.value)}} 
+			// 		onKeyDown={writetarea} value={newtarea} placeholder="Añadir nueva tarea"/></li>
+			// 	{tareas.map((tarea,index) => {
+			// 			return (<li className="list-group-item" key={index}>{tarea}</li>)
+			// 		}
+			// 		)}	
+			// 	<li class="list-group-item"><div className="contador"><span>{tareas.length} Tareas</span></div></li>
+			// </ul>
