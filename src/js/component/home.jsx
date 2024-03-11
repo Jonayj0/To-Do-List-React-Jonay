fab975a2b8eb3d8309140fb5a0c475d629a32dd9
Jonayj0 <141333392+Jonayj0@users.noreply.github.com>
2024-03-11 13:52:50 +0000

    Lista Tareas React Jonay

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -4,40 +4,43 @@ import React, { useState } from "react";
 //create your first component
 const Home = () => {
 
- const [newtarea,setNewtarea]=useState("")
- const [tareas,setTareas]=useState([])
+ const [newtask,setNewtask]=useState("")
+ const [tasks,settasks]=useState([])
  
-
- function writetarea(event) {
-	// console.log(event);
-	if (event.key === "Enter") {
-		// console.log("Agregar tarea");
-		setTareas(tareas.concat(newtarea))
-		setNewtarea("");
+////// -------------Esta funcion añade una task nueva en el Array al presionar ENTER----------------------////
+	function writetask(event) {
+		// console.log(event);
+		if (event.key === "Enter") {
+			// console.log("Agregar task");
+			settasks(tasks.concat(newtask))
+			setNewtask("");
+		}
 	}
- }
+////// -------------Esta funcion añade una una task nueva en el Array al presionar ENTER----------------------////
 
-// function deleteTarea(event) {
-// 	console.log(event);
-// }
 
+ ////// -------------Esta funcion elimina una position del Array por su indice----------------------////
+	function deletetask(position) {
+		const arrayfiltered = tasks.filter((item, index) => index !== position)
+		settasks(arrayfiltered)
+		}
+////// -------------Esta funcion elimina una position del Array por su indice----------------------////
 
 	return (
 		<>
 		<h1 className="text-center mt-5"><b>LISTA DE TAREAS</b></h1>
-		<div className="container border">
-			<input className="list container border-0 py-3" type="text" onChange={(event) => {setNewtarea(event.target.value)}} 
-					onKeyDown={writetarea} value={newtarea} placeholder="Añadir nueva tarea"/>
+		<div className="blog container">
+			<input className="list container border-0 py-3" type="text" 
+					onChange={(event) => {setNewtask(event.target.value)}} 
+					onKeyDown={writetask} value={newtask} placeholder="Añadir nueva tarea"/>
 					
 			<ul className="list-group list-group-flush">
-					{tareas.map((tarea,index) => {
-						return (<li className="list-group-item py-3 ms-3" 
-						key={index} onClick={(event) => {}}>{tarea} 
-						<button className="btn text-danger border-0"><i className="fa-solid fa-xmark" ></i></button></li>)
+					{tasks.map((task,index) => { return (<li className="list-group-item py-3 ms-3" key={index}> {task}   
+						<span className="delete hidden" onClick={() => deletetask(index)}> <i className="fa-solid fa-xmark" ></i></span></li>)
 					}
 					)}	
 			</ul>
-				<div className="contador border-top p-3"><span>{tareas.length} Tareas</span></div>	
+				<div className="contador border-top p-3"><span>{tasks.length} tasks</span></div>	
 		</div>
 		</>
 	);
@@ -47,14 +50,10 @@ export default Home;
 
 
 
+//  function deletetask(indextask) {
+// 	const taskEliminada = tasks.filter((task, index) => index !== indextask);
+// 	settasks(taskEliminada);
+// 	}
 
 
-			// <ul class="list-group m-5">
-			// 	<li class="list-group-item"><input className="list container border-0" type="text" onChange={(event) => {setNewtarea(event.target.value)}} 
-			// 		onKeyDown={writetarea} value={newtarea} placeholder="Añadir nueva tarea"/></li>
-			// 	{tareas.map((tarea,index) => {
-			// 			return (<li className="list-group-item" key={index}>{tarea}</li>)
-			// 		}
-			// 		)}	
-			// 	<li class="list-group-item"><div className="contador"><span>{tareas.length} Tareas</span></div></li>
-			// </ul>
+{/* <button className="btn text-danger border-0" onClick={() => deletetask(index)}><i className="fa-solid fa-xmark" ></i></button> */}
