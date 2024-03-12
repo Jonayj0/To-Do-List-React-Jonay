3ead8243c9b6e3435fbf53a5a23a46f309013548
Jonayj0 <141333392+Jonayj0@users.noreply.github.com>
2024-03-12 12:22:07 +0000

    TODO LIST React Jonay Final

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -4,35 +4,32 @@ import React, { useState } from "react";
 //create your first component
 const Home = () => {
 
- const [newtask,setNewtask]=useState("")
- const [tasks,settasks]=useState([])
+ const [newTask,setNewTask]=useState("")
+ const [tasks,setTasks]=useState([])
  
 ////// -------------Esta funcion añade una task nueva en el Array al presionar ENTER----------------------////
-	function writetask(event) {
+	function writeTask(event) {
 		// console.log(event);
 		if (event.key === "Enter") {
 			// console.log("Agregar task");
-			settasks(tasks.concat(newtask))
-			setNewtask("");
+			setTasks(tasks.concat(newTask))
+			setNewTask("");
 		}
 	}
-////// -------------Esta funcion añade una una task nueva en el Array al presionar ENTER----------------------////
-
 
  ////// -------------Esta funcion elimina una position del Array por su indice----------------------////
 	function deletetask(position) {
 		const arrayfiltered = tasks.filter((item, index) => index !== position)
-		settasks(arrayfiltered)
+		setTasks(arrayfiltered)
 		}
-////// -------------Esta funcion elimina una position del Array por su indice----------------------////
 
 	return (
 		<>
 		<h1 className="text-center mt-5"><b>LISTA DE TAREAS</b></h1>
 		<div className="blog container">
 			<input className="list container border-0 py-3" type="text" 
-					onChange={(event) => {setNewtask(event.target.value)}} 
-					onKeyDown={writetask} value={newtask} placeholder="Añadir nueva tarea"/>
+					onChange={(event) => {setNewTask(event.target.value)}} 
+					onKeyDown={writeTask} value={newTask} placeholder="Añadir nueva tarea"/>
 					
 			<ul className="list-group list-group-flush">
 					{tasks.map((task,index) => { return (<li className="list-group-item py-3 ms-3" key={index}> {task}   
